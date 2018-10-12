6b44f0be28ddd46bd2211978935f156ce1f6b389
Þorbjörg Hekla <Hekla@orbjorgs-MacBook-Pro.local>
2018-10-12 20:24:47 +0000

    test fyrir eina tölu
    test fyrir eina tölu

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,11 @@ const add = require("./calculator");
 it("should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
+
+it("should return 1 if the string have only the number 1", () => {
+    expect(add("1")).toBe(1);
+});
+
+it("should return 12 if the stirng have only the number 12", () => {
+    expect(add("12")).toBe(12);
+});
