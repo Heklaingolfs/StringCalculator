2a7035b0d17f6517c57e051b3e7583500fd06467
Þorbjörg Hekla <Hekla@orbjorgs-MacBook-Pro.local>
2018-10-12 21:49:30 +0000

    Höndla New Lines

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,12 @@
 function add(number){
-    
-    if(number == "")
+
+    if(number == "" || number == "\n")
     {
         return 0;
     }
-    var numberlist = number.split(",");
+    // Fann aðferð hér "https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript"
+    var noNewLine = number.replace(/\n/g, ",");
+    var numberlist = noNewLine.split(",");
     var sum = 0;
     for(var i = 0; i < numberlist.length; i++) {
         sum += Number(numberlist[i]);
